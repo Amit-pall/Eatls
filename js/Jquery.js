cb3d54533296f62ec01f33ffd0c3516bfdea51d4
Amit-pall <88040738+Amit-pall@users.noreply.github.com>
2021-08-07 22:30:19 +0600

    Add files via upload

---
+++ js/Jquery.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($){
+    jQuery(".isotope").isotope();
+});
+
+        // menu area start
+        
+        $(function() {
+            $("i.fas.fa-bars").click(function () {
+                $(".main-manu ul").toggleClass("all");
+            });
+
+
+            
+        });
